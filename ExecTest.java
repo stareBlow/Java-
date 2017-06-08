6b573ac26a4b9a32df6dd0b380097b7a43987d9a
孟晨 <1293141942@qq.com>
2017-06-08 14:08:03 +0800

    open notepad

--- ExecTest.java
+++ ExecTest.java
@@ -4,4 +4,12 @@ package Java基础库类;
  * Created by 孟晨 on 2017/6/8.
  */
 public class ExecTest {
+    public static void main(String[] args)
+        throws Exception
+    {
+        Runtime rt = Runtime.getRuntime();
+        //运行记事本程序
+        rt.exec("notepad.exe");
+    }
+
 }
