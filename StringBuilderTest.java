070275e0c2f2d4c2157a66b0ebcca6f6f451e0bc
孟晨 <1293141942@qq.com>
2017-06-09 15:28:04 +0800

    可修改的字符串

---
+++ StringBuilderTest.java
@@ -0,0 +1,24 @@
+package Java基础库类;
+
+/**
+ * Created by 孟晨 on 2017/6/9.
+ */
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        StringBuilder sc = new StringBuilder();
+        // 追加字符
+        sc.append("java");  //sc = "java"
+        sc.insert(0, "hello "); //sc = "helllo java"
+        sc.replace(5, 6, ",");  //sc = "hello,java"
+        sc.delete(5, 6);    //sc = "hellojava"
+        System.out.println(sc);
+        //反转
+        sc.reverse();
+        System.out.println("反转：" + sc);
+        System.out.println(sc.length());
+        System.out.println(sc.capacity());
+
+
+
+    }
+}
