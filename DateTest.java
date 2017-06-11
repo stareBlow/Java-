25e871622261bbf837624d91e9f7572913d053cc
孟晨 <1293141942@qq.com>
2017-06-11 16:38:29 +0800

    Date类测试

--- DateTest.java
+++ DateTest.java
@@ -1,7 +1,17 @@
 package Java基础库类;
 
+import java.util.*;
+
 /**
  * Created by 孟晨 on 2017/6/10.
  */
 public class DateTest {
+    public static void main(String[] args) {
+        Date d1 = new Date();
+        // 获取当前时间之后的100ms时间
+        Date d2 = new Date(System.currentTimeMillis() + 100);
+        System.out.println(d2);
+        System.out.println(d1.compareTo(d2));
+        System.out.println(d1.before(d2));
+    }
 }
