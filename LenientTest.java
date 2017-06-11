799323fef9057903fce101de8a94d0e42a979cb2
孟晨 <1293141942@qq.com>
2017-06-11 16:41:14 +0800

    Calendar容错性

--- LenientTest.java
+++ LenientTest.java
@@ -1,7 +1,21 @@
 package Java基础库类;
+import static java.util.Calendar.*;
+import java.util.Calendar;
+
 
 /**
  * Created by 孟晨 on 2017/6/10.
  */
 public class LenientTest {
+    public static void main(String[] args) {
+        Calendar ca1 = Calendar.getInstance();
+        // 结果YEAR字段加1，MONTH字段为1（2）
+        ca1.set(MONTH, 13);
+        System.out.println(ca1.getTime());
+        // 关闭容错性
+        ca1.setLenient(false);
+        // 运行时是异常
+        ca1.set(MONTH, 13);
+        System.out.println(ca1.getTime());
+    }
 }
